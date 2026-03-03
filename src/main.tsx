0d43420d195e20d196f4292f16ff03800b241b0c
nirmalagit config --global user.email nirmala <nirmalaprachande6091@gmail.comgit config --global user.name nirmala>
2026-03-04 04:34:05 +0530

    fix: force dark mode for deployed app

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeScript,
+  extendTheme,
+} from "@chakra-ui/react";
 import { registerSW } from "virtual:pwa-register";
 import App from "./App";
 
 registerSW({ immediate: true });
 
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <ColorModeScript initialColorMode="light" />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
